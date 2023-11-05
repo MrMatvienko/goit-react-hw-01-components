48afa00e2aebdf1e886bf834fb7c9649aa46e06d
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2023-11-05 23:53:42 +0200

    comit3

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+// import React from 'react';
+// import PropTypes from 'prop-types';
 
-export const Profile = ({ username, avatar, tag, location, stats }) => {
-  return (
-    <ProfileCard>
-      <Describtion>
-        <Avatar src={avatar} alt="User avatar" />
-        <Name>{username}</Name>
-        <Tag>{tag}</Tag>
-        <Location>{location}</Location>
-      </Describtion>
-      <StatsList>
-        <StatsItem>
-          <Label>Followers</Label>
-          <Quantity>{stats.followers}</Quantity>
-        </StatsItem>
-        <StatsItem>
-          <Label>Views</Label>
-          <Quantity>{stats.views}</Quantity>
-        </StatsItem>
-        <StatsItem>
-          <Label>Likes</Label>
-          <Quantity>{stats.likes}</Quantity>
-        </StatsItem>
-      </StatsList>
-    </ProfileCard>
-  );
-};
+// export const Profile = ({ username, avatar, tag, location, stats }) => {
+//   return (
+//     <ProfileCard>
+//       <Describtion>
+//         <Avatar src={avatar} alt="User avatar" />
+//         <Name>{username}</Name>
+//         <Tag>{tag}</Tag>
+//         <Location>{location}</Location>
+//       </Describtion>
+//       <StatsList>
+//         <StatsItem>
+//           <Label>Followers</Label>
+//           <Quantity>{stats.followers}</Quantity>
+//         </StatsItem>
+//         <StatsItem>
+//           <Label>Views</Label>
+//           <Quantity>{stats.views}</Quantity>
+//         </StatsItem>
+//         <StatsItem>
+//           <Label>Likes</Label>
+//           <Quantity>{stats.likes}</Quantity>
+//         </StatsItem>
+//       </StatsList>
+//     </ProfileCard>
+//   );
+// };
 
-Profile.propTypes = {
-  username: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  stats: PropTypes.shape({
-    followers: PropTypes.number.isRequired,
-    views: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired,
-  }).isRequired,
-};
+// Profile.propTypes = {
+//   username: PropTypes.string.isRequired,
+//   avatar: PropTypes.string.isRequired,
+//   tag: PropTypes.string.isRequired,
+//   location: PropTypes.string.isRequired,
+//   stats: PropTypes.shape({
+//     followers: PropTypes.number.isRequired,
+//     views: PropTypes.number.isRequired,
+//     likes: PropTypes.number.isRequired,
+//   }).isRequired,
+// };
